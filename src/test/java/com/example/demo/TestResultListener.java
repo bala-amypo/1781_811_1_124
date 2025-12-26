c93ce3a72f01e020daf60d0306070d5ddcf7e719
bala-amypo <bala@amypo.in>
2025-12-26 04:34:56 +0000

    User commit

--- src/test/java/com/example/demo/TestResultListener.java
+++ src/test/java/com/example/demo/TestResultListener.java
@@ -8,31 +8,31 @@ public class TestResultListener implements ITestListener {
 
     @Override
     public void onStart(ITestContext context) {
-        System.out.println("=== Test Suite Started: " + context.getName() + " ===");
+        System.out.println("===== Test Suite Started =====");
     }
 
     @Override
     public void onFinish(ITestContext context) {
-        System.out.println("=== Test Suite Finished: " + context.getName() + " ===");
+        System.out.println("===== Test Suite Finished =====");
     }
 
     @Override
     public void onTestStart(ITestResult result) {
-        System.out.println("STARTED: " + result.getName());
+        System.out.println("TEST STARTED: " + result.getName());
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
-        System.out.println("PASSED: " + result.getName());
+        System.out.println("TEST PASSED: " + result.getName());
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
-        System.out.println("FAILED: " + result.getName());
+        System.out.println("TEST FAILED: " + result.getName());
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
-        System.out.println("SKIPPED: " + result.getName());
+        System.out.println("TEST SKIPPED: " + result.getName());
     }
 }
