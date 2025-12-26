83fd32e1f67133623cb72b80e5ec8839ec760baf
bala-amypo <bala@amypo.in>
2025-12-26 04:02:48 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class DemoApplication {
+@SpringBootTest
+public class DemoApplicationTests {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+    @Test
+    void contextLoads() {
+        // This test passes if Spring Boot starts correctly
     }
 }
